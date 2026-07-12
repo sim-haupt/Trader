2626418bd82c72e19287844a940b870cc02d763a
szym000 <165960056+szym000@users.noreply.github.com>
2026-07-12 22:09:35 +0200

    wip

--- prisma/manual/20260712_update_tax_report_defaults.sql
+++ prisma/manual/20260712_update_tax_report_defaults.sql
@@ -2,3 +2,13 @@ ALTER TABLE "TaxSetting"
   ALTER COLUMN "exchangeRateSource" SET DEFAULT 'Automatic USD/EUR FX API',
   ALTER COLUMN "matchingMethod" SET DEFAULT 'COMPLETED_ROUND_TRIP',
   ALTER COLUMN "reconciliationTolerance" SET DEFAULT 0.01;
+
+UPDATE "TaxSetting"
+SET "exchangeRateSource" = 'Automatic USD/EUR FX API',
+    "matchingMethod" = CASE
+      WHEN "matchingMethod" = 'FIFO' THEN 'COMPLETED_ROUND_TRIP'
+      ELSE "matchingMethod"
+    END,
+    "updatedAt" = NOW()
+WHERE "exchangeRateSource" IN ('Manual ECB EUR/USD CSV', 'Manual EUR/USD CSV', 'frankfurter.dev USD/EUR')
+   OR "matchingMethod" = 'FIFO';
